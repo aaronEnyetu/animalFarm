9651412d6428a542543134c92e96bd6f5b50adf6
Aaron Enyetu <aaronenyetu@gmail.com>
2022-05-09 13:42:00 -0700

    added missing id keyvalue

---
+++ animalFarm/animalfarm.js
@@ -0,0 +1,16 @@
+import { findById } from '../utils.js';
+import { animals } from '../data.js';
+
+const animalImage = document.getElementById('animal-name');
+const animalName = document.getElementById('animal-image');
+const animalQuote = document.getElementById('animal-quote');
+
+const params = new URLSearchParams(window.location.search);
+console.log(params.get('id'), animals);
+
+const farmAnimals = findById(params.get('id'), animals);
+console.log(farmAnimals);
+
+animalName.textContent = farmAnimals.name;
+animalImage.src = `../assets/${farmAnimals.type}.svg`;
+animalQuote.textContent = farmAnimals.says;
